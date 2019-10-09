6bd8b4f0085afe66f370ee6126fee9ce0c87ce86
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-10-09 15:20:06 -0600

    snipcart test 2

--- server/Controllers/checkoutController.js
+++ server/Controllers/checkoutController.js
@@ -6,7 +6,7 @@ module.exports = {
         // where cust_id = cust_id from req.session.user.cust_id
         // it returns sum of card prices
         const db = req.app.get('db')
-        const {cust_id} = req.params
+        const {cust_id} = req.session.user
         let result = await db.get_yearly_drop_total(cust_id)
         // a new var divides it by 100
         let total = result[0] / 100
