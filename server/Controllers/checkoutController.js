4ccf95be5b6ca7c925f34e9b4407691fb59f476d
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-10-09 15:35:02 -0600

    snipcart test 6

--- server/Controllers/checkoutController.js
+++ server/Controllers/checkoutController.js
@@ -5,7 +5,7 @@ module.exports = {
         const {total} = req.params
         let toSend = {
             "id": "carddrop-yearly-drop",
-            "price": this.getYearlyDropTotal,
+            "price": +total,
             "url": `https://thecarddrop.com/api/checkout/yearly-drop/${total}`
         }
         res.status(200).send(toSend)
