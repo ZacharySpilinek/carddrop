737356625bdb41ad9715bd6c2f3d254f49b09f3a
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-10-09 23:55:59 -0600

    snipcart test 30

--- server/Controllers/testController.js
+++ server/Controllers/testController.js
@@ -1,6 +1,7 @@
 module.exports = {
     updateSubId: async (db, webhook) => {
         let {email} = webhook.content.user
+        console.log(email)
         const result = await db.find_user({email})
         console.log(result)
     }
