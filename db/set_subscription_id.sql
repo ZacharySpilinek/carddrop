44ec4cccdc77a3d5386fa9d45fe700c80022a219
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-10-10 00:12:24 -0600

    snipcart test 35

--- db/set_subscription_id.sql
+++ db/set_subscription_id.sql
@@ -1,3 +1,4 @@
 UPDATE customer
-SET sub_id = $1
+SET sub_id = $1,
+sub_interval = $3
 WHERE email = $2;
--- server/Controllers/testController.js
+++ server/Controllers/testController.js
@@ -3,7 +3,11 @@ module.exports = {
         let {email} = webhook.content.user
         const result = await db.find_user_by_email(email)
         if (result[0]) {
-            await db.set_subscription_id([webhook.content.id, email])
+            let interval = ''
+            if (webhook.content.schedule.interval === "Year"){
+                interval = 'yearly'
+            }
+            await db.set_subscription_id([webhook.content.id, email, interval])
         } else {
             console.log('no user found under that email')
         }
