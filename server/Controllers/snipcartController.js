8fa05a9557c9af284365d751a87dcb40521f14e3
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-10-09 22:56:43 -0600

    snipcart test 10

--- server/Controllers/snipcartController.js
+++ server/Controllers/snipcartController.js
@@ -3,7 +3,7 @@ module.exports = {
     snipcartWebhook: (req, res, next) => {
         const db = req.app.get('db')
         if (req.body.eventName === "subscription.created"){
-            test()
+            updateSubId(req.body)
         }
         res.sendStatus(200)
     },
@@ -12,16 +12,16 @@ module.exports = {
         axios.get('https://app.snipcart.com/api/orders', {auth: {username: API_KEY, password: ''}}).then(result => {
             res.status(200).send(result.data)
         }).catch(err => console.log(`Error: ${err}`))
-    },
+    }/* ,
     updateSubId: async (req, res, next) => {
         console.log(req)
         // const db = req.app.get('db')
         // const user = await db.find_user(req.body.content.user.email)
         // const {cust_id} = user
         // await db.set_subscription_id()
-    }
+    } */
 }
 
-test = () => {
-    console.log('test')
+updateSubId = (webhook) => {
+    console.log(webhook)
 }
