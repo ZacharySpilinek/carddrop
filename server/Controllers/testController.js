f8808620d78c25c159f12ebd27e0d8020c32d7b8
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-10-09 23:44:46 -0600

    snipcart test 26

--- server/Controllers/testController.js
+++ server/Controllers/testController.js
@@ -1,6 +1,6 @@
 module.exports = {
     test: async (prm, db) => {
-        const result = db.find_user('zachary.spilinek@gmail.com')
+        const result = await db.find_user('zachary.spilinek@gmail.com')
         console.log(result)
     }
 }
