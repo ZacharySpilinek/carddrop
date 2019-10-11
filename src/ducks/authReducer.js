018660e953cfafd976e4bb2c0b9f1535e55693bd
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-10-11 16:33:52 -0600

    pre-reducer splitting

---
+++ src/ducks/authReducer.js
@@ -0,0 +1,66 @@
+const initialState = {
+    cust_id: null,
+    email: '',
+    first_name: '',
+    last_name: '',
+    sub_id: null,
+    sub_interval: ''
+}
+
+const SET_USER_ID = "SET_USER_ID"
+const GET_USER = "GET_USER"
+const CLEAR_STATE = "CLEAR_STATE"
+
+export const setUserId = (userInfo) => {
+    return {
+        type: SET_USER_ID,
+        payload: userInfo
+    }
+}
+
+export const getUser = (userInfo) => {
+    return {
+        type: GET_USER,
+        payload: userInfo
+    }
+}
+
+export const clearState = () => {
+    return {
+        type: CLEAR_STATE
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case GET_USER:
+            return {...state,
+                cust_id: action.payload.cust_id,
+                email: action.payload.email,
+                first_name: action.payload.first_name,
+                last_name: action.payload.last_name,
+                sub_id: action.payload.sub_id,
+                sub_interval: action.payload.sub_interval}
+        case SET_USER_ID:
+                return {...state,
+                    cust_id: action.payload.cust_id,
+                    email: action.payload.email,
+                    first_name: action.payload.first_name,
+                    last_name: action.payload.last_name,
+                    sub_id: action.payload.sub_id,
+                    sub_interval: action.payload.sub_interval}
+        case CLEAR_STATE:
+                return {...state,
+                    cust_id: null,
+                    email: '',
+                    first_name: '',
+                    last_name: '',
+                    sub_id: null,
+                    sub_interval: '',
+                }
+        default:
+            return state
+    }
+}
+
+export default reducer
